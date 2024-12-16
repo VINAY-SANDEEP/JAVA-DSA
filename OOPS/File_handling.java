92f758c82ca7204d1d6982121b7188fcaf8f942b
Vinay-Sandeep <133085003+VINAY-SANDEEP@users.noreply.github.com>
2024-12-16 11:18:40 +0530

    Update File_handling.java

--- OOPS/File_handling.java
+++ OOPS/File_handling.java
@@ -3,25 +3,13 @@ import java.io.*;
 public class File_handling {
   public static void main(String[] args) throws IOException {
   File f = new File("C:\\Users\\rsrin\\Desktop\\listening\\vishnu");
-  // System.out.println(f.createNewFile());
-  // System.out.println(f.delete());
-  // System.out.println(f.mkdir());
-  // System.out.println(f.delete());
-  //System.out.println(f.exists());
-  //  if (!f.exists()) {
-  //   f.createNewFile();
-  //  }
-  //  FileInputStream sai = new FileInputStream(f);
-  //  int a;
-  //  while ((a = sai.read()) != -1) {
-  //   System.out.print((char) a);
-  //  }
-// try {
-//   FileWriter nuvvu = new FileWriter(f);
-//   nuvvu.write("priya nuvva leka nenu lenu");
-//   nuvvu.close();
-// } catch (Exception e) {
-//   System.out.println("nike radhu ra ");
-// }
+   FileInputStream sai = new FileInputStream(f);
+   int a;
+  while ((a = sai.read()) != -1) {
+   System.out.print((char) a);
+  }
+   FileWriter nuvvu = new FileWriter(f);
+  nuvvu.write("priya nuvva leka nenu lenu");
+   nuvvu.close();
   }
 }
