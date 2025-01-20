e7dbe2bca67784dc53070853efb0c3cfa51f7bdb
Vinay-Sandeep <133085003+VINAY-SANDEEP@users.noreply.github.com>
2025-01-20 09:33:09 +0530

    Update Single.java

--- DSA/LinkedList/Single.java
+++ DSA/LinkedList/Single.java
@@ -159,6 +159,29 @@ public class Single {
         }
         return node;
     }
+        Node sortedMerge(Node head1, Node head2) {
+        if (head1 == null) return head2;
+        if (head2 == null) return head1;
+        Node dummy = new Node(0);
+        Node tail = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        if (head1 != null) {
+            tail.next = head1;
+        } else {
+            tail.next = head2;
+        }
+        return dummy.next;
+        
+    }
         public static void main(String[] args) {
            Single x = new Single();
             x.insertFirst(10);
